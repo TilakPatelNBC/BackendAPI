1c3530ead9203d6c4c2a077df04ba24d7b70acb0
Tilak <tilak.patel@nbcuni.com>
2019-08-06 09:25:31 -0400

    Remove externals

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
-var nodeExternals = require('webpack-node-externals');
-
 module.exports = {
   webpack: (config) => {
     config.node = {
       fs: 'empty'
     }
     return config
-  },
-  externals: [nodeExternals()]
+  }
 };
