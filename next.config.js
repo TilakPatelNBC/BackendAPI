301bb11b37daac59c984adc6eebe990e5472ddfe
Tilak <tilak.patel@nbcuni.com>
2019-08-06 09:23:19 -0400

    Remove yarn lock

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
+var nodeExternals = require('webpack-node-externals');
+
 module.exports = {
   webpack: (config) => {
     config.node = {
       fs: 'empty'
     }
     return config
-  }
+  },
+  externals: [nodeExternals()]
 };
