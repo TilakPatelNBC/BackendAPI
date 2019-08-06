0b27ab909186cb8ea4d2c1728cab200e42750cc9
Tilak <tilak.patel@nbcuni.com>
2019-08-06 11:54:24 -0400

    Herokuuu

--- pages/api/user/update-user.js
+++ pages/api/user/update-user.js
@@ -1,12 +1,12 @@
-import {update} from '../../../services/mongo/mongoService'
-import send from '../../../services/sendStatus'
+import {update} from '../../../services/mongo/mongoService';
+import send from '../../../services/sendStatus';
 
 export default (req, res) => {
     res.setHeader('Content-Type', 'application/json')
     if (req && req.body && req.body.user && req.body.update){
         update('users', req.body.user, req.body.update);
-        send(res, 200, {'updated_user': req.body.update})
+        send(res, 200, {'updated_user': req.body.update});
     } else {
-        send(res, 400, {'error': 'no body found'})
+        send(res, 400, {'error': 'no body found'});
     }
 }
